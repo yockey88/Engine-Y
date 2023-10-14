6b21c0ddcd47dcfb204d127286b559fb25d828f6
Yock <64506168+yockey88@users.noreply.github.com>
2023-10-14 01:57:06 -0700

    fixed pating issue

--- launcher/enginey_launcher.cpp
+++ launcher/enginey_launcher.cpp
@@ -4,8 +4,32 @@ class Launcher : public YE::App {
     public:
         Launcher() : YE::App() {}
         virtual ~Launcher() {}
+        
+        virtual YE::WindowConfig GetWindowConfig() override {
+            YE::WindowConfig config;
+            config.title = "Engine Y";
+            config.size.x = 1920;
+            config.size.y = 1080;
+            config.clear_color = { 0.1f , 0.1f , 0.1f , 1.f };
+            config.fullscreen = false;
+            config.vsync = false;
+            config.rendering_to_screen = true;
+            config.flags |= SDL_WINDOW_RESIZABLE;
+            return config;
+        }
 
-        virtual void RenderGui() {
+        virtual void Initialize() override {
+            EngineY::RegisterKeyPressCallback(
+                [&](YE::KeyPressed* event) -> bool {
+                    if (event->Key() == YE::Keyboard::Key::YE_ESCAPE)
+                        EngineY::DispatchEvent(ynew YE::ShutdownEvent);
+                    return true;
+                } ,
+                "editor-keys"
+            );
+        }
+
+        virtual void DrawGui() {
             ImGui::Begin("EngineY Launcher");
             ImGui::Text("Hello World!");
             ImGui::End();
@@ -13,5 +37,5 @@ class Launcher : public YE::App {
 };
 
 YE::App* CreateApp() {
-    return new Launcher();
+    return ynew Launcher();
 }
