aaa4ca305c0764adfbfdce8c7e1575661bf3379d
Yock <64506168+yockey88@users.noreply.github.com>
2023-10-17 19:23:45 -0700

    fixed typo

--- engine/include/core/window_manager.hpp
+++ engine/include/core/window_manager.hpp
@@ -3,7 +3,22 @@
 
 namespace YE {
 
-    
+    class WindowManager {
+        static WindowManager* singleton;
+
+        WindowManager() {}
+        ~WindowManager() {}
+
+        WindowManager(WindowManager&&) = delete;
+        WindowManager(const WindowManager&) = delete;
+        WindowManager& operator=(WindowManager&&) = delete;
+        WindowManager& operator=(const WindowManager&) = delete;
+
+        public:
+            static WindowManager* Instance();
+
+            void Cleanup();
+    };
 
 }
 
--- engine/src/core/window_manager.cpp
+++ engine/src/core/window_manager.cpp
@@ -1,7 +1,19 @@
-#include "window_manager.hpp"
+#include "core/window_manager.hpp"
 
 namespace YE {
 
+    WindowManager* WindowManager::singleton = nullptr;
 
-    
+    WindowManager* WindowManager::Instance() {
+        if (singleton == nullptr) {
+            singleton = new WindowManager;
+        }
+        return singleton;
+    }
+
+    void WindowManager::Cleanup() {
+        delete singleton;
+        singleton = nullptr;
+    }
+
 }
